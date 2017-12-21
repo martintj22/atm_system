179b824215ec8f8125043e458334b1e6b2298532
HOTSKPLAP048\cyg_server <satoz.game@gmail.com>
2017-12-21 10:15:12 +0100

    Netbank

---
+++ Netbank/login.php
@@ -0,0 +1,63 @@
+<?php session_start(); ?>
+<html>
+<head>
+	<title>Login</title>
+</head>
+
+<body>
+<?php
+include("connection.php");
+
+if(isset($_POST['submit'])) {
+	$user = mysqli_real_escape_string($mysqli, $_POST['username']);
+	$pass = mysqli_real_escape_string($mysqli, $_POST['password']);
+
+	if($user == "" || $pass == "") {
+		echo "Forkerte oplysninger.";
+		echo "<br/>";
+		echo "<a href='login.php'>Go back</a>";
+	} else {
+		$result = mysqli_query($mysqli, "SELECT * FROM login WHERE username='$user' AND password=md5('$pass')")
+					or die("Could not execute the select query.");
+		
+		$row = mysqli_fetch_assoc($result);
+		
+		if(is_array($row) && !empty($row)) {
+			$validuser = $row['username'];
+			$_SESSION['valid'] = $validuser;
+			$_SESSION['name'] = $row['name'];
+			$_SESSION['id'] = $row['id'];
+		} else {
+			echo "Invalid username or password.";
+			echo "<br/>";
+			echo "<a href='login.php'>Go back</a>";
+		}
+
+		if(isset($_SESSION['valid'])) {
+			header('Location: edit.php');			
+		}
+	}
+} else {
+?>
+	<p><font size="+2">Hæveautomat</font></p>
+	<form name="form1" method="post" action="">
+		<table width="75%" border="0">
+			<tr> 
+				<td width="10%">Kortnr</td>
+				<td><input type="text" name="username"></td>
+			</tr>
+			<tr> 
+				<td>Pincode</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr> 
+				<td>&nbsp;</td>
+				<td><input type="submit" name="submit" value="Submit"></td>
+			</tr>
+		</table>
+	</form>
+<?php
+}
+?>
+</body>
+</html>
---
+++ Netbank/view.php
@@ -0,0 +1,51 @@
+<?php session_start(); ?>
+
+<?php
+if(!isset($_SESSION['valid'])) {
+	header('Location: login.php');
+}
+?>
+
+<?php
+//including the database connection file
+include_once("connection.php");
+
+//fetching data in descending order (lastest entry first)
+$result = mysqli_query($mysqli, "SELECT * FROM products WHERE login_id=".$_SESSION['id']." ORDER BY id DESC");
+?>
+
+
+
+
+<html>
+<head>
+	<title>Homepage</title>
+</head>
+
+
+
+<body>
+	<a href="index.php">Netbank</a>  | <a href="logout.php">Logud</a>
+	<br/><br/>
+	
+	<table width='80%' border=0>
+		<tr bgcolor='#CCCCCC'>
+			<td>Navn</td>
+			<td>Saldo</td>
+		</tr>
+		<?php
+		while($res = mysqli_fetch_array($result)) {		
+			echo "<tr>";
+			echo "<td>".$res['name']."</td>";
+			echo "<td>".$res['qty']."</td>";
+		  echo "<td><a href=\"edit.php?id=$res[id]\">500.kr</a> | </td>";		
+
+		}
+		?>
+	</table>
+
+	
+
+</body>
+</html>
+
