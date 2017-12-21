a119bbe0d0e3e746e0e93fe92df1a30c66298ac4
HOTSKPLAP053\Bjorn__Valhalla <mlc4447@hotmail.com>
2017-12-21 14:45:06 +0100

    her er C# Platformen og dens tilbehør

--- Netbank/view.php
+++ Netbank/view.php
@@ -11,7 +11,7 @@ if(!isset($_SESSION['valid'])) {
 include_once("connection.php");
 
 //fetching data in descending order (lastest entry first)
-$result = mysqli_query($mysqli, "SELECT * FROM products WHERE login_id=".$_SESSION['id']." ORDER BY id DESC");
+$result = mysqli_query($mysqli, "SELECT * FROM testtable WHERE Card_number=".$_SESSION['id']." ORDER BY id DESC");
 ?>
 
 
