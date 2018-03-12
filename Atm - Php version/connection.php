2ef5f586d9309c4c2e32ff5cb87ee518fd09069b
Martin Jørgensen <satoz.game@gmail.com>
2018-03-12 15:01:35 +0100

    Update connection.php

--- Atm - Php version/connection.php
+++ Atm - Php version/connection.php
@@ -1,16 +1,6 @@
 <?php
 /*
-// mysql_connect("database-host", "username", "password")
-$conn = mysql_connect("localhost","root","root") 
-			or die("cannot connected");
-
-// mysql_select_db("database-name", "connection-link-identifier")
-@mysql_select_db("test2",$conn);
-*/
-
-/**
- * mysql_connect is deprecated
- * using mysqli_connect instead
+// Opretter forbindelse til databasen
  */
 
 $databaseHost = 'localhost';
